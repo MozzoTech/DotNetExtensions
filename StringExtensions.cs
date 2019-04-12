bcfe5b2fd95c6b93400a6196f2f461cee952381d
MozzoTech <humair@writeme.com>
2019-04-12 17:52:13 +0500

    Update StringExtensions.cs

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace Humair.Extensions
 {
 public static class StringExtensions
